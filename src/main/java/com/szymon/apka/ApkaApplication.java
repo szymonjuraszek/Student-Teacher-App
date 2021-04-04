a219cdf67576a22b1d4001e083d3afcb1f8a0d53
Szymon Juraszek <szymonjuraszek777@gmail.com>
2021-04-04 17:08:04 +0200

    Init commit

---
+++ src/main/java/com/szymon/apka/ApkaApplication.java
@@ -0,0 +1,13 @@
+package com.szymon.apka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApkaApplication.class, args);
+    }
+
+}
